e0068a0e75275cd2bac899304ff74a48c102e7c1
Huertz <104065687+Huertz@users.noreply.github.com>
2023-12-06 02:53:42 -0600

    rendering item list

--- src/PackingList.jsx
+++ src/PackingList.jsx
@@ -1,3 +1,31 @@
+/* eslint-disable react/prop-types */
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: true },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+];
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <span>X</span>
+    </li>
+  );
+}
+
 export default function PackingList() {
-  return <div className='list'>LIST</div>;
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <Item
+            item={item}
+            key='desciption'
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
